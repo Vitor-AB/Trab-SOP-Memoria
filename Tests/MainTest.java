10ee18ec57f100b77c4338ff566a9759b4536698
Vitor AB <88466135+Vitor-AB@users.noreply.github.com>
2022-11-23 00:10:26 -0300

    Update MainTest

--- Tests/MainTest.java
+++ Tests/MainTest.java
@@ -6,12 +6,8 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
-    /*@org.junit.jupiter.api.Test
-    void findSubFIFO() {
-    }*/
-
-    @org.junit.jupiter.api.Test
+    // TESTES FIFO
+    @Test
     @DisplayName("FIFO Test 1")
     void  FIFOarraytest1() {
         ArrayList<FrameM> test = new ArrayList<>();
@@ -98,18 +94,114 @@ class MainTest {
         test.add(f4);
 
         ArrayList<FrameM> memoria = new ArrayList<>();
+        memoria.add(f1);
+        memoria.add(f6);
         memoria.add(f8);
         memoria.add(f7);
-        memoria.add(f8);
-        memoria.add(f2);
         memoria.add(f2);
         memoria.add(f4);
         System.out.println(memoria);
         assertEquals(memoria, Main.FIFO(test,6));
     }
 
+    // TESTES LFU
+    @Test
+    @DisplayName("LFU Test 1")
+    void  LFUarraytest1() {
+        ArrayList<FrameM> test = new ArrayList<>();
+        FrameM f1 = new FrameM(1,10,15,0,0);
+        FrameM f2 = new FrameM(2,1,20,1,1);
+        FrameM f3 = new FrameM(3,2,12,1,0);
+        FrameM f4 = new FrameM(4,11,21,0,1);
+        FrameM f5 = new FrameM(5,21,10,0,0);
+        FrameM f6 = new FrameM(6,26,52,1,1);
+        FrameM f7 = new FrameM(7,15,36,1,0);
+        FrameM f8 = new FrameM(8,12,18,0,1);
+        test.add(f1);
+        test.add(f2);
+        test.add(f3);
+        test.add(f4);
+        test.add(f5);
+        test.add(f6);
+        test.add(f7);
+        test.add(f8);
+        test.add(f3);
+        test.add(f4);
 
-    /*@org.junit.jupiter.api.Test
-    void main() {
-    }*/
+        ArrayList<FrameM> memoria = new ArrayList<>();
+        memoria.add(f5);
+        memoria.add(f6);
+        memoria.add(f7);
+        memoria.add(f4);
+        System.out.println(memoria);
+        assertEquals(memoria, Main.LFU(test,4));
+    }
+    @Test
+    @DisplayName("LFU Test 2")
+    void  LFUarraytest2() {
+        ArrayList<FrameM> test = new ArrayList<>();
+        FrameM f1 = new FrameM(1,10,15,0,0);
+        FrameM f2 = new FrameM(2,1,20,1,1);
+        FrameM f3 = new FrameM(3,2,12,1,0);
+        FrameM f4 = new FrameM(4,11,21,0,1);
+        FrameM f5 = new FrameM(5,21,10,0,0);
+        FrameM f6 = new FrameM(6,26,52,1,1);
+        FrameM f7 = new FrameM(7,15,36,1,0);
+        FrameM f8 = new FrameM(8,12,18,0,1);
+        test.add(f2);
+        test.add(f6);
+        test.add(f4);
+        test.add(f8);
+        test.add(f3);
+        test.add(f7);
+        test.add(f1);
+        test.add(f5);
+        test.add(f6);
+        test.add(f8);
+
+        ArrayList<FrameM> memoria = new ArrayList<>();
+        memoria.add(f6);
+        memoria.add(f4);
+        memoria.add(f8);
+        memoria.add(f7);
+        memoria.add(f5);
+        System.out.println(memoria);
+        assertEquals(memoria, Main.LFU(test,5));
+    }
+    @Test
+    @DisplayName("LFU Test 3")
+    void  LFUarraytest3() {
+        ArrayList<FrameM> test = new ArrayList<>();
+        FrameM f1 = new FrameM(1,10,15,0,0);
+        FrameM f2 = new FrameM(2,1,20,1,1);
+        FrameM f3 = new FrameM(3,2,12,1,0);
+        FrameM f4 = new FrameM(4,11,21,0,1);
+        FrameM f5 = new FrameM(5,21,10,0,0);
+        FrameM f6 = new FrameM(6,26,52,1,1);
+        FrameM f7 = new FrameM(7,15,36,1,0);
+        FrameM f8 = new FrameM(8,12,18,0,1);
+        test.add(f3);
+        test.add(f1);
+        test.add(f1);
+        test.add(f6);
+        test.add(f8);
+        test.add(f7);
+        test.add(f8);
+        test.add(f2);
+        test.add(f2);
+        test.add(f4);
+
+        ArrayList<FrameM> memoria = new ArrayList<>();
+        memoria.add(f3);
+        memoria.add(f1);
+        memoria.add(f6);
+        memoria.add(f8);
+        memoria.add(f7);
+        memoria.add(f4);
+        System.out.println(memoria);
+        assertEquals(memoria, Main.LFU(test,6));
+    }
+
+    // TESTES LRU
+
 }
