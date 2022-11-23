3a99b91b22563ddbc9f77d9747e4343e0ea276d3
Vitor AB <88466135+Vitor-AB@users.noreply.github.com>
2022-11-23 00:09:57 -0300

    Update Main

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import algSubstituicao.FIFO;
 import java.util.ArrayList;
 
 public class Main {
-
-    public static int FindSubFIFO(ArrayList<FrameM> memoria){
+    //----------------------------------------------------------------------
+    // FIFO - First-in First-out
+    public static int FindsubFIFO(ArrayList<FrameM> memoria){
         int minNum = memoria.get(0).getTempCarga();
         for (FrameM i: memoria) {
             if(i.getTempCarga() < minNum){
@@ -15,51 +16,161 @@ public class Main {
         for (FrameM i: memoria) {
             if(minNum == i.getTempCarga()){
                 ID = i.getID();
+                break;
             }
         }
 
         return ID;
     }
-
     public static ArrayList<FrameM> FIFO(ArrayList<FrameM> frames, int limPag){
         ArrayList<FrameM> memoria = new ArrayList<>();
         int aux = 1;
         for (FrameM i: frames){
             i.setTempCarga(aux);
-
-            if (memoria.size() != limPag) {
+            boolean namemo = false;
+            if(memoria.size() != 0){
+                for (FrameM k: memoria) {
+                    if (i == k) {
+                        namemo = true;
+                        break;
+                    }
+                }
+            }
+            if (memoria.size() != limPag && !namemo) {
                 memoria.add(i);
+
             }else{
-                int sub = FindSubFIFO(memoria);
+                int sub = FindsubFIFO(memoria);
                 for (int j = 0; j < memoria.size(); j++){
-                    if(memoria.get(j).getID() == sub){
+                    if(memoria.get(j).getID() == sub && !namemo){
                         memoria.remove(j);
                         memoria.add(i);
                         break;
                     }
                 }
             }
-            aux += 1;
         }
-        System.out.println(memoria);
+        aux += 1;
+        System.out.println("Memoria: " +memoria);
         return memoria;
     }
 
-    /*public static ArrayList<FrameM> LFR(ArrayList<FrameM> frames, int limPag){
+    //----------------------------------------------------------------------
+    // LFU - Least-Frequently-Used
+    public static int FindsubLFU(ArrayList<FrameM> memoria){
+        int minNum = memoria.get(0).getQuanRef();
+        for (FrameM i: memoria) {
+            if(i.getQuanRef() < minNum){
+                minNum = i.getQuanRef();
+            }
+        }
+        int ID = 0;
+        for (FrameM i: memoria) {
+            if(minNum == i.getQuanRef()){
+                ID = i.getID();
+                break;
+            }
+        }
+        return ID;
+    }
+    public static ArrayList<FrameM> LFU(ArrayList<FrameM> frames, int limPag){
+        ArrayList<FrameM> memoria = new ArrayList<>();
+        for (FrameM i: frames) {
+            boolean namemo = false;
+            if(memoria.size() != 0){
+                for (FrameM k: memoria) {
+                    if (i == k) {
+                        namemo = true;
+                        break;
+                    }
+                }
+            }
+            if (memoria.size() != limPag && !namemo) {
+                memoria.add(i);
+            }else {
+                int id = FindsubLFU(memoria);
+                for (int j = 0; j < memoria.size(); j++) {
+                    if (memoria.get(j).getID() == id && !namemo){
+                        memoria.remove(j);
+                        memoria.add(i);
+                        break;
+                    }
+                }
+            }
+            System.out.println("Memoria: " +memoria);
+        }
 
-    }*/
+        return memoria;
+    }
+
+    //----------------------------------------------------------------------
+    // LRU - Least-Recently-Used
+    public static int FindsubLRU(ArrayList<FrameM> memoria){
+        int minNum = memoria.get(0).getTempo_UltRef();
+        for (FrameM i: memoria) {
+            if(i.getTempo_UltRef() < minNum){
+                minNum = i.getTempo_UltRef();
+            }
+        }
+        int ID = 0;
+        for (FrameM i: memoria) {
+            if(minNum == i.getTempo_UltRef()){
+                ID = i.getID();
+                break;
+            }
+        }
+        return ID;
+    }
+    public static ArrayList<FrameM> LRU(ArrayList<FrameM> frames, int limPag){
+        ArrayList<FrameM> memoria = new ArrayList<>();
+        int aux = 1;
+        for (FrameM i: frames) {
+            i.setTempo_UltRef(aux);
+            boolean namemo = false;
+            if(memoria.size() != 0){
+                for (FrameM k: memoria) {
+                    if (i == k) {
+                        namemo = true;
+                        i.setTempo_UltRef(aux + 1);
+                        break;
+                    }
+                }
+            }
+            if (memoria.size() != limPag && !namemo) {
+                memoria.add(i);
+            }else {
+                int id = FindsubLRU(memoria);
+                for (int j = 0; j < memoria.size(); j++) {
+                    if (memoria.get(j).getID() == id && !namemo){
+                        memoria.remove(j);
+                        memoria.add(i);
+                        break;
+                    }
+                }
+            }
+            if (aux + 1 == i.getTempo_UltRef()){
+                aux += 2;
+            } else {
+                aux += 1;
+            }
+
+            System.out.println("Memoria: " +memoria);
+        }
+
+        return memoria;
+    }
 
     public static void main(String[] args) {
         ArrayList<FrameM> frames = new ArrayList<>();
         int limPag = 4;
-        FrameM f1 = new FrameM(1,10,15,0,0);
-        FrameM f2 = new FrameM(2,1,20,1,1);
-        FrameM f3 = new FrameM(3,2,12,1,0);
-        FrameM f4 = new FrameM(4,11,21,0,1);
-        FrameM f5 = new FrameM(5,21,10,0,0);
-        FrameM f6 = new FrameM(6,26,52,1,1);
-        FrameM f7 = new FrameM(7,15,36,1,0);
-        FrameM f8 = new FrameM(8,12,18,0,1);
+        FrameM f1 = new FrameM(1,10,0,0,0);
+        FrameM f2 = new FrameM(2,1,0,1,1);
+        FrameM f3 = new FrameM(3,2,0,1,0);
+        FrameM f4 = new FrameM(4,11,0,0,1);
+        FrameM f5 = new FrameM(5,21,0,0,0);
+        FrameM f6 = new FrameM(6,26,0,1,1);
+        FrameM f7 = new FrameM(7,15,0,1,0);
+        FrameM f8 = new FrameM(8,12,0,0,1);
         frames.add(f3);
         frames.add(f1);
         frames.add(f1);
@@ -70,7 +181,10 @@ public class Main {
         frames.add(f2);
         frames.add(f2);
         frames.add(f4);
-        System.out.println(frames);
-        System.out.println(FIFO(frames,6));
+        System.out.println("Frames: " + frames);
+
+        //FIFO(frames,4);
+        //LFU(frames,6);
+        LRU(frames,4);
     }
 }
